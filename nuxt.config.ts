230a148d6203347ecb461ef9c7e6c7b758bcad5f
addykonen <konenadjana@gmail.com>
2025-08-16 13:29:29 +0800

    fix: nuxt config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,10 +2,8 @@
 export default defineNuxtConfig({
   nitro: {
     static: true,
-    routeRules: {
-      "/**": { prerender: true },
-    },
   },
+  
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   modules: ["@nuxt/ui", "@nuxt/eslint", "@nuxt/image"],
